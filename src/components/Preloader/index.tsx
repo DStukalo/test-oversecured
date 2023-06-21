322d2494e8ceaf067274033310ee059438b79585
DStukalo <94228608+DStukalo@users.noreply.github.com>
2023-06-21 19:06:56 +0300

    feat: add preloader

---
+++ src/components/Preloader/index.tsx
@@ -0,0 +1,11 @@
+import styles from "./Preloader.module.css";
+
+export const Preloader = () => {
+  return (
+    <div className={styles.preloader}>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  );
+};
