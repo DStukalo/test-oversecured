340dae97713423f563c88d3e0bab4ee93d1c24e2
DStukalo <94228608+DStukalo@users.noreply.github.com>
2023-06-21 20:18:41 +0300

    refactor: update _app add fonts and layout

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,18 @@ import { SessionProvider } from "next-auth/react";
 import { type AppType } from "next/app";
 import { api } from "~/utils/api";
 import "~/styles/globals.css";
+import { Inter, Roboto } from "next/font/google";
+import Layout from "~/components/Layout";
+
+const inter = Inter({ subsets: ["latin"], variable: "--font-inter" });
+
+const roboto = Roboto({
+  weight: ["400", "700"],
+  style: ["normal", "italic"],
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-roboto",
+});
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
@@ -10,7 +22,11 @@ const MyApp: AppType<{ session: Session | null }> = ({
 }) => {
   return (
     <SessionProvider session={session}>
-      <Component {...pageProps} />
+      <div className={`${roboto.variable} ${inter.variable}`}>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </div>
     </SessionProvider>
   );
 };
